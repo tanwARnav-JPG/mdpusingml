fa07d3de1295be41569ae2a9bc25dedb1430b18b
tanwARnav-JPG <arnavtanwar18@gmail.com>
2025-02-24 14:51:56 +0000

    mdp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,268 @@
+import pickle
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('C:/Users/arnav/Desktop/Major Project/Major Project/Trained Model/diabetes_model1.sav', 'rb'))
+
+heart_disease_model = pickle.load(open('C:/Users/arnav/Desktop/Major Project/Major Project/Trained Model/heart_disease_model.sav','rb'))
+
+parkinsons_model = pickle.load(open('C:/Users/arnav/Desktop/Major Project/Major Project/Trained Model/parkinsons_model.sav', 'rb'))
+
+
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Menu Driven Multiple Disease Prediction',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        gender = st.text_input('Gender(Male-0/Female-1)')
+    
+    with col3:
+        polyuria = st.text_input('Excess Urination(Y-1/N-0)')
+    
+    with col1:
+        polydipsia = st.text_input('Excess thirst or not(Y-1/N-0)')
+    
+    with col2:
+        sudden_weight_loss = st.text_input('Sudden Weight Loss(Y-1/N-0)')
+    
+    with col3:
+        weakness = st.text_input('Weakness(Y-1/N-0)')
+    
+    with col1:
+        polyphagia = st.text_input('Episode of Excess Hunger(Y-1/N-0)')
+        
+    with col2:
+        genital_thrush = st.text_input('Yeast Infection(Y-1/N-0)')
+    
+    with col3:
+        visual_blurring = st.text_input('Blurred Vision(Y-1/N-0)')
+    
+    with col1:
+        itching = st.text_input('Episode of Itching(Y-1/N-0)')
+        
+    with col2:
+        irritability = st.text_input('Episode of Irritability(Y-1/N-0)')
+    
+    with col3:
+        delayed_healing = st.text_input('Delayed Healing of Wound(Y-1/N-0)')
+    
+    with col1:
+        partial_paresis = st.text_input('Episode of Muscle Weakening(Y-1/N-0)')
+        
+    with col2:
+        muscle_stiffness = st.text_input('Episode of muscle stiffness(Y-1/N-0)')
+    
+    with col3:
+        alopecia = st.text_input('Hair Loss(Y-1/N-0)')
+    
+    with col1:
+        obesity = st.text_input('Obesity(Y-1/N-0)')
+        
+    
+    
+    
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[age, gender, polyuria, polydipsia, sudden_weight_loss, weakness, polyphagia, genital_thrush,visual_blurring,
+                                                   itching,irritability,delayed_healing,partial_paresis,muscle_stiffness,alopecia,obesity]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+
+
+
+
+# Heart Disease Prediction Page
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        sex = st.text_input('Sex')
+        
+    with col3:
+        cp = st.text_input('Chest Pain types')
+        
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+        
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+        
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+        
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+        
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+        
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+        
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+        
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+        
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        
+        
+     
+     
+    # code for Prediction
+    heart_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = heart_disease_model.predict([[int(age),int(sex),int(cp),int(trestbps),int(chol), int(fbs), 
+                                                         int(restecg), int(thalach),int(exang),int(oldpeak),int(slope),
+                                                         int(ca),int(thal)]])                          
+        
+        if (heart_prediction[0] == 1):
+          heart_diagnosis = 'The person is having heart disease'
+        else:
+          heart_diagnosis = 'The person does not have any heart disease'
+        
+    st.success(heart_diagnosis)
+        
+    
+    
+
+# Parkinson's Prediction Page
+if (selected == "Parkinsons Prediction"):
+    
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+        
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+        
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+        
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+        
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+        
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+        
+    
+    
+    # code for Prediction
+    parkinsons_diagnosis = ''
+    
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
+        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
+        
+        if (parkinsons_prediction[0] == 1):
+          parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+          parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        
+    st.success(parkinsons_diagnosis)
+
+
